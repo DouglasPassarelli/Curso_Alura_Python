65f86c53e67f8c32b4f2567a3eca0c546364cd20
DouglasPassarelli <doga.passarelli@hotmail.com>
2023-05-20 23:52:27 -0300

    feat:Incrementando-o-projeto

--- Avançando_na_Linguagem/forca.py
+++ Avançando_na_Linguagem/forca.py
@@ -8,6 +8,19 @@ def jogar():
 
     menu('Bem vindo ao jogo da forca!')
 
+    palavra_secreta = "banana"
+    acertou = False
+    enforcou = False
+
+    while not acertou and not enforcou:
+        chute = input('Qual a letra? ').strip().upper()
+
+        for index, letra in enumerate(palavra_secreta):
+            if chute == letra.upper():
+                print(f'Encontrei a letra {chute} na posição {index}')
+
+        print("jogando...")
+
 
 if __name__ == "__main__":
     jogar()
