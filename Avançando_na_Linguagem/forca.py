3b05ef2dd195ce8937ea38c2295115d9d3d8d6eb
DouglasPassarelli <doga.passarelli@hotmail.com>
2023-05-22 22:38:30 -0300

    feat:Melhorando a visibilidade do nosso jogo

--- Avançando_na_Linguagem/forca.py
+++ Avançando_na_Linguagem/forca.py
@@ -5,21 +5,39 @@ def jogar():
         print(msg)
         print('*' * tam)
 
+    def letras_disponiveis(list, indece):
+        """print(*list)"""
+        i = list.index(indece)
+        list[i] = '*'
+        return list
+
+
+
 
     menu('Bem vindo ao jogo da forca!')
 
+    letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+              'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+              'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Z']
     palavra_secreta = "banana"
+    letras_acertadas = ['_', '_', '_', '_', '_', '_']
     acertou = False
     enforcou = False
 
+    """print(*letras_disponiveis)"""
+    print('Letras disponiveis:', *letras)
+    print(*letras_acertadas)
     while not acertou and not enforcou:
         chute = input('Qual a letra? ').strip().upper()
-
+        disponiveis = letras_disponiveis(letras, chute)
+        print('Letras disponiveis:', *disponiveis)
         for index, letra in enumerate(palavra_secreta):
             if chute == letra.upper():
-                print(f'Encontrei a letra {chute} na posição {index}')
+                letras_acertadas[index] = letra
 
-        print("jogando...")
+        print(*letras_acertadas)
+
+    print('Fim do Jogo')
 
 
 if __name__ == "__main__":
