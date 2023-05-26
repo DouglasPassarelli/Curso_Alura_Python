e4e36b1a898abdc262c66e836d3dbbfbc75023ed
DouglasPassarelli <doga.passarelli@hotmail.com>
2023-05-26 00:24:55 -0300

    feat:Adicionando nova funcionalidades

--- Avançando_na_Linguagem/forca.py
+++ Avançando_na_Linguagem/forca.py
@@ -1,4 +1,8 @@
 def jogar():
+    def linhas(tam=27):
+        print('*' * tam)
+
+
     def menu(msg, tam=0):
         tam = len(msg)
         print('*' * tam)
@@ -6,7 +10,6 @@ def jogar():
         print('*' * tam)
 
     def letras_disponiveis(list, indece):
-        """print(*list)"""
         if indece in list:
             i = list.index(indece)
             list[i] = '*'
@@ -15,29 +18,51 @@ def jogar():
             print('Esta letra ja foi selecionada, Por favor digite outra!')
             print(f'Letras Disponiveis:', *list)
 
-
     menu('Bem vindo ao jogo da forca!')
 
     letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
               'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
               'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Z']
-    palavra_secreta = "banana"
-    letras_acertadas = ['_', '_', '_', '_', '_', '_']
+    palavra_secreta = "maça".upper()
+    letras_acertadas = ['_' for letra in palavra_secreta]
     acertou = False
     enforcou = False
+    tentativa = 6
+    rodadas = 0
 
-
+    print(f'A Palavra chave tem {len(palavra_secreta)} letras.')
     print('Letras disponiveis:', *letras)
     print('Palavra-Chave:', *letras_acertadas)
+    linhas()
 
 
     while not acertou and not enforcou:
         chute = input('Qual a letra? ').strip().upper()
         letras_disponiveis(letras, chute)
-        for index, letra in enumerate(palavra_secreta):
-            if chute == letra.upper():
-                letras_acertadas[index] = letra
+        if chute in palavra_secreta:
+            print('Voce acertou!!!')
+            for index, letra in enumerate(palavra_secreta):
+                if chute == letra.upper():
+                    letras_acertadas[index] = letra
+            print(f'Ainda restam {letras_acertadas.count("_")} letras')
+        else:
+            tentativa -= 1
+            print(f'Voce errou, ainda restam {tentativa} tentativas!')
+
         print('Palavra-chave:', *letras_acertadas)
+        rodadas += 1
+        linhas()
+        acertou = '_' not in letras_acertadas
+        enforcou = tentativa == 0
+
+    if acertou:
+        print('Parabens voce ganhou o jogo da forca.')
+        print(f'Voce ganhou com {rodadas} rodadas')
+        print(f'Palavra chave: {palavra_secreta} ')
+    else:
+        print('Voce perdeu, Mais sorte da proxima veiz!')
+        print(f'A palavra chave era {palavra_secreta}')
+        print('Ate a proxima!')
 
 
     print('Fim do Jogo')
